a127d073d8debc5954e1cb4149e77c60b22acdb1
8ight1een <2633510260@qq.com>
2026-09-25 20:59:36 +0800

    feat:新增接口数据模型文件

---
+++ backend/schemas.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, Field,EmailStr, StringConstraints
+from typing import Literal, Annotated
+from datetime import date
+from decimal import Decimal
+
+Name = Annotated[str, StringConstraints(strip_whitespace=True, min_length=1, max_length=100)]
+
+
+#请求模型
+class TransactionCreate(BaseModel):
+    account_id: int = Field(gt=0)
+    category_id: int = Field(gt=0)
+    money: Decimal = Field(gt=0, max_digits=9, decimal_places=2, allow_inf_nan=False)
+    money_type: Literal["收入", "支出"]
+    description: str | None = Field(default=None, max_length=250)
+    transaction_date: date
+
+
+class AccountCreate(BaseModel):
+    name_accounts: Name
+
+
+class CategoryCreate(BaseModel):
+    name_categories: Name
+    money_type: Literal["收入", "支出"]
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str=Field(min_length=8, max_length=32)
+    nickname: Annotated[str, StringConstraints(strip_whitespace=True, min_length=1, max_length=20)]
+
